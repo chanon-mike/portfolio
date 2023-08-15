9dc8579c1bd03b4b46f0b7c5ed71bdd102737d87
chanon-mike <zzmikezz8520@gmail.com>
2023-08-15 19:41:57 +0900

    feat: overview section

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,6 @@
-import '@/pages/globals.css';
 import type { AppProps } from 'next/app';
 import { Ubuntu } from 'next/font/google';
+import './globals.css';
 
 const ubuntu = Ubuntu({ weight: ['300', '400', '500', '700'], subsets: ['latin'] });
 
---
+++ src/utils/motion.ts
@@ -0,0 +1,43 @@
+export const textVariant = (delay?: number) => {
+  return {
+    hidden: {
+      y: -50,
+      opacity: 0,
+    },
+    show: {
+      y: 0,
+      opacity: 1,
+      transition: {
+        type: 'spring',
+        duration: 1.25,
+        delay,
+      },
+    },
+  };
+};
+
+export const fadeIn = (
+  direction: '' | 'left' | 'right' | 'up' | 'down',
+  type: string,
+  delay: number,
+  duration: number
+) => {
+  return {
+    hidden: {
+      x: direction === 'left' ? 100 : direction === 'right' ? -100 : 0,
+      y: direction === 'up' ? 100 : direction === 'down' ? -100 : 0,
+      opacity: 0,
+    },
+    show: {
+      x: 0,
+      y: 0,
+      opacity: 1,
+      transition: {
+        type,
+        delay,
+        duration,
+        ease: 'easeOut',
+      },
+    },
+  };
+};

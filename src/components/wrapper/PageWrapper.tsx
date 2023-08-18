7c1391789be07d9ecb38ba69d32861f5a8ffe19f
chanon-mike <zzmikezz8520@gmail.com>
2023-08-18 21:27:04 +0900

    feat: update mt for project detail

--- src/components/wrapper/PageWrapper.tsx
+++ src/components/wrapper/PageWrapper.tsx
@@ -7,7 +7,7 @@ const PageWrapper = (Component: () => JSX.Element) => {
         initial="hidden"
         whileInView="show"
         viewport={{ once: true, amount: 0.25 }}
-        className="mt-14 mb-10 lg:w-3/4 xl:w-1/2 lg:mx-auto md:mx-20 mx-5 text-text flex flex-col justify-center"
+        className="mt-20 mb-10 lg:w-3/4 xl:w-1/2 lg:mx-auto md:mx-20 mx-5 text-text flex flex-col justify-center"
       >
         <Component />
       </motion.div>
